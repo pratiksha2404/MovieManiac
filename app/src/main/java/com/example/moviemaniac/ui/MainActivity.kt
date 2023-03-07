26e9ad5faad0b3c02fc21f06bcfb01fa53f3540a
pratiksha <pshindeps448@gmail.com>
2023-03-07 23:46:50 +0530

    Hide bottom navigation bar for loginFragment, RegisterFragment and MovieDetailsFragment.

--- app/src/main/java/com/example/moviemaniac/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviemaniac/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.moviemaniac.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviemaniac.R
@@ -22,6 +23,18 @@ class MainActivity : AppCompatActivity()
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navHostController = navHostFragment.navController
         binding.bottomNavigationBar.setupWithNavController(navHostController)
+        navHostController.addOnDestinationChangedListener{_, destination, _ ->
+            if( destination.id == R.id.loginFragment ||
+                    destination.id == R.id.registerFragment ||
+                    destination.id == R.id.moviesDetailsFragment )
+            {
+                binding.bottomNavigationBar.visibility = View.GONE
+            }
+            else
+            {
+                binding.bottomNavigationBar.visibility = View.VISIBLE
+            }
+        }
     }
 
 }
